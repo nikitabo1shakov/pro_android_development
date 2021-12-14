0bf9827c284197a1fe62ca3c8eb2579cb8f91362
nikitabolshakov <nikitas.bolshakov@gmail.com>
2021-12-14 20:28:54 +0300

    Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.nikitabolshakov.proandroiddevelopment"
+        minSdk = 24
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    // Design
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    // Kotlin
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.12.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+    // Koin
+    implementation("io.insert-koin:koin-core:3.1.2")
+    implementation("io.insert-koin:koin-android:3.1.2")
+    implementation("io.insert-koin:koin-android-compat:3.1.2")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    kapt("com.github.bumptech.glide:compiler:4.11.0")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.3.0")
+    kapt("androidx.room:room-compiler:2.3.0")
+    implementation("androidx.room:room-ktx:2.3.0")
+
+    // Test
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,38 @@
+object Versions {
+
+    // Design
+    const val APPCOMPAT_VERSION = "1.4.0"
+    const val MATERIAL_VERSION = "1.4.0"
+    const val CONSTRAINT_LAYOUT_VERSION = "2.1.2"
+    const val SWIPEREFRESH_LAYOUT_VERSION = "1.1.0"
+
+    // Kotlin
+    const val CORE_VERSION = "1.7.0"
+    const val STD_LIB_VERSION = "1.6.0"
+    const val COROUTINES_CORE_VERSION = "1.5.2"
+    const val COROUTINES_ANDROID_VERSION = "1.5.2"
+
+    // Retrofit
+    const val RETROFIT_VERSION = "2.9.0"
+    const val CONVERTER_GSON_VERSION = "2.9.0"
+    const val INTERCEPTOR_VERSION = "3.12.1"
+    const val ADAPTER_COROUTINES_VERSION = "0.9.2"
+
+    // Koin
+    const val KOIN_CORE_VERSION = "3.1.2"
+    const val KOIN_ANDROID_VERSION = "3.1.2"
+    const val KOIN_ANDROID_COMPAT_VERSION = "3.1.2"
+
+    // Glide
+    const val GLIDE_VERSION = "4.11.0"
+
+    // Room
+    const val ROOM_KTX_VERSION = "2.3.0"
+    const val ROOM_RUNTIME_VERSION = "2.3.0"
+    const val ROOM_COMPILER_VERSION = "2.3.0"
+
+    // Test
+    const val JUNIT_VERSION = "4.13.2"
+    const val RUNNER_VERSION = "1.4.0"
+    const val ESPRESSO_CORE_VERSION = "3.4.0"
+}
